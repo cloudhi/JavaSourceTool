c678e073c0729574118f6ff8999e601ffeec6f3e
陈小锅 <yoojiachen@gmail.com>
2015-03-10 09:56:30 +0800

    版权信息外置到其它文件

--- Readme.md
+++ Readme.md
@@ -2,4 +2,12 @@
 
 ## Usage
 
+### Step 1
+
+在 COPYRIGHT_TEXT.txt 添加或者修改版权信息。如果文件不存在，请创建
+
+### Step 2
+
+在命令行下运行
+
 	$python copyright.py 'your-java-source-root-dir'
--- copyright.py
+++ copyright.py
@@ -3,18 +3,13 @@ __author__ = 'Yoojia.Chen@gmail.com'
 import os
 import sys
 
-my_copyright = """/*
- * Copyright (c) 2015. Xi'an iRain IOT Technology Service CO.,Ltd. All Rights Reserved.
- */
-"""
-
 
 # 插入版本信息
-def insert_copyright(java_file):
+def insert_copyright(matched_copyright, java_file):
     f = open(java_file, 'r')
     contents = f.readlines()
     f.close()
-    contents.insert(0, my_copyright)
+    contents.insert(0, matched_copyright)
     f = open(java_file, 'w')
     contents = ''.join(contents)
     f.write(contents)
@@ -22,7 +17,7 @@ def insert_copyright(java_file):
 
 
 # 判断Java文件的版本信息是否匹配
-def find_copyright(java_file):
+def find_copyright(matched_copyright, java_file):
     f = open(java_file)
     cr_buf = []
     # 获取 package 之前的代码
@@ -41,14 +36,14 @@ def find_copyright(java_file):
         # Exists
         copyright_exists,
         # Matched
-        copyright_exists and my_copyright in ''.join(cr_buf),
+        copyright_exists and matched_copyright in ''.join(cr_buf),
         # Content
         content
     )
 
 
 # 处理版权信息
-def perform_copyright(java_file):
+def perform_copyright(java_file, my_copyright):
     rs = find_copyright(java_file)
     # Copyright exists and matched
     if rs[1]:
@@ -57,7 +52,7 @@ def perform_copyright(java_file):
     # Not exists, insert into java file
     elif not rs[0]:
         print '+ 添加版权声明到此Java源文件: %s' % java_file
-        insert_copyright(java_file)
+        insert_copyright(java_file, my_copyright)
     # Exists , but not matched
     else:
         print '> 此Java源文件已存在其它版本信息: %s' % java_file
@@ -65,13 +60,23 @@ def perform_copyright(java_file):
 
 # 遍历Java源文件目录
 def walk(java_path):
-    print '> Walking Java Source path: %s' % java_path
+    if not os.path.isfile('COPYRIGHT_TEXT.txt'):
+        print '- COPYRIGHT_TEXT.txt is Required !'
+
+    cr_f = open('COPYRIGHT_TEXT.txt', 'r')
+    my_copyright = ''.join(cr_f.readlines())
+    cr_f.close()
+    print "---- Using Copyright ----"
+    print my_copyright
+    print '-------------------------'
+    print '> Java Source path: %s' % java_path
     for dir_path, _, names in os.walk(java_path):
         for name in names:
             if not name.endswith('.java'):
                 continue
             java_file = dir_path + os.sep + name
-            perform_copyright(java_file)
+            perform_copyright(java_file, my_copyright)
+
 
 # 主入口
 if __name__ == '__main__':
